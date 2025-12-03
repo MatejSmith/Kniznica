06b79f9ed3bd61d77e232d913f3aaea33fb2fa29
Matej Kováč <mtjkovac@gmail.com>
2025-12-03 13:10:20 +0100

    Initial commit

---
+++ BackEnd/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is an Express.js app.');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
